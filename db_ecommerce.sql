030ee8818d47718f90f97f8168372b2dbe869b16
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-05-20 00:39:33 +0300

    .

--- db_ecommerce.sql
+++ db_ecommerce.sql
@@ -1,1 +1,598 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2025 at 11:37 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messagein`
+--
+
+CREATE TABLE `messagein` (
+  `Id` int(11) NOT NULL,
+  `SendTime` datetime DEFAULT NULL,
+  `ReceiveTime` datetime DEFAULT NULL,
+  `MessageFrom` varchar(80) DEFAULT NULL,
+  `MessageTo` varchar(80) DEFAULT NULL,
+  `SMSC` varchar(80) DEFAULT NULL,
+  `MessageText` text DEFAULT NULL,
+  `MessageType` varchar(80) DEFAULT NULL,
+  `MessageParts` int(11) DEFAULT NULL,
+  `MessagePDU` text DEFAULT NULL,
+  `Gateway` varchar(80) DEFAULT NULL,
+  `UserId` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messagelog`
+--
+
+CREATE TABLE `messagelog` (
+  `Id` int(11) NOT NULL,
+  `SendTime` datetime DEFAULT NULL,
+  `ReceiveTime` datetime DEFAULT NULL,
+  `StatusCode` int(11) DEFAULT NULL,
+  `StatusText` varchar(80) DEFAULT NULL,
+  `MessageTo` varchar(80) DEFAULT NULL,
+  `MessageFrom` varchar(80) DEFAULT NULL,
+  `MessageText` text DEFAULT NULL,
+  `MessageType` varchar(80) DEFAULT NULL,
+  `MessageId` varchar(80) DEFAULT NULL,
+  `ErrorCode` varchar(80) DEFAULT NULL,
+  `ErrorText` varchar(80) DEFAULT NULL,
+  `Gateway` varchar(80) DEFAULT NULL,
+  `MessageParts` int(11) DEFAULT NULL,
+  `MessagePDU` text DEFAULT NULL,
+  `Connector` varchar(80) DEFAULT NULL,
+  `UserId` varchar(80) DEFAULT NULL,
+  `UserInfo` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messageout`
+--
+
+CREATE TABLE `messageout` (
+  `Id` int(11) NOT NULL,
+  `MessageTo` varchar(80) DEFAULT NULL,
+  `MessageFrom` varchar(80) DEFAULT NULL,
+  `MessageText` text DEFAULT NULL,
+  `MessageType` varchar(80) DEFAULT NULL,
+  `Gateway` varchar(80) DEFAULT NULL,
+  `UserId` varchar(80) DEFAULT NULL,
+  `UserInfo` text DEFAULT NULL,
+  `Priority` int(11) DEFAULT NULL,
+  `Scheduled` datetime DEFAULT NULL,
+  `ValidityPeriod` int(11) DEFAULT NULL,
+  `IsSent` tinyint(1) NOT NULL DEFAULT 0,
+  `IsRead` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblautonumber`
+--
+
+CREATE TABLE `tblautonumber` (
+  `ID` int(11) NOT NULL,
+  `AUTOSTART` varchar(11) NOT NULL,
+  `AUTOINC` int(11) NOT NULL,
+  `AUTOEND` int(11) NOT NULL,
+  `AUTOKEY` varchar(12) NOT NULL,
+  `AUTONUM` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblautonumber`
+--
+
+INSERT INTO `tblautonumber` (`ID`, `AUTOSTART`, `AUTOINC`, `AUTOEND`, `AUTOKEY`, `AUTONUM`) VALUES
+(1, '2017', 1, 74, 'PROID', 10),
+(2, '0', 1, 102, 'ordernumber', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `CATEGID` int(11) NOT NULL,
+  `CATEGORIES` varchar(255) NOT NULL,
+  `USERID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`CATEGID`, `CATEGORIES`, `USERID`) VALUES
+(5, 'SHOES', 0),
+(11, 'BAGS', 0),
+(12, 'CLOTHING', 0),
+(13, 'INTERIORS', 0),
+(14, 'HOUSEHOLDS', 0),
+(15, 'FASHION', 0),
+(16, 'KIDS', 0),
+(17, 'WOMENS', 0),
+(18, 'MENS', 0),
+(19, 'SPORTSWEAR', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcustomer`
+--
+
+CREATE TABLE `tblcustomer` (
+  `CUSTOMERID` int(11) NOT NULL,
+  `FNAME` varchar(30) NOT NULL,
+  `LNAME` varchar(30) NOT NULL,
+  `MNAME` varchar(30) NOT NULL,
+  `CUSHOMENUM` varchar(90) NOT NULL,
+  `STREETADD` text NOT NULL,
+  `CITY` text NOT NULL,
+  `CITYADD` text NOT NULL,
+  `PROVINCE` varchar(80) NOT NULL,
+  `COUNTRY` varchar(30) NOT NULL,
+  `DBIRTH` date NOT NULL,
+  `GENDER` varchar(10) NOT NULL,
+  `PHONE` varchar(20) NOT NULL,
+  `EMAILADD` varchar(40) NOT NULL,
+  `ZIPCODE` int(6) NOT NULL,
+  `CUSUNAME` varchar(20) NOT NULL,
+  `CUSPASS` varchar(90) NOT NULL,
+  `CUSPHOTO` varchar(255) NOT NULL,
+  `TERMS` tinyint(4) NOT NULL,
+  `DATEJOIN` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcustomer`
+--
+
+INSERT INTO `tblcustomer` (`CUSTOMERID`, `FNAME`, `LNAME`, `MNAME`, `CUSHOMENUM`, `STREETADD`, `CITY`, `CITYADD`, `PROVINCE`, `COUNTRY`, `DBIRTH`, `GENDER`, `PHONE`, `EMAILADD`, `ZIPCODE`, `CUSUNAME`, `CUSPASS`, `CUSPHOTO`, `TERMS`, `DATEJOIN`) VALUES
+(16, 'solen', 'd', '', '', '', 'Addis Ababa', 'Addis Ababa', '', '', '0000-00-00', 'Male', '+251991898259', '', 0, 'solen', 'd53df297eabec2f5a3cb3a09164f168acbd02866', '', 1, '2025-05-19'),
+(17, 'siso', 't', '', '', '', '', 'Addis Ababa', '', '', '0000-00-00', 'Male', '+251991898259', '', 0, 'siso', 'd53df297eabec2f5a3cb3a09164f168acbd02866', '', 1, '2025-05-19'),
+(18, 'what', 'j', '', '', '', '', 'Addis Ababa', '', '', '0000-00-00', 'Female', '+251991898259', '', 0, 'siso', 'd53df297eabec2f5a3cb3a09164f168acbd02866', '', 1, '2025-05-19'),
+(19, 'solan', 'a', '', '', '', '', 'Addis Ababa', '', '', '0000-00-00', 'Male', '+251991898259', '', 0, 'siso', 'acd7236e31641b4de86fd7af037655976d76c9c6', '', 1, '2025-05-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblorder`
+--
+
+CREATE TABLE `tblorder` (
+  `ORDERID` int(11) NOT NULL,
+  `PROID` int(11) NOT NULL,
+  `ORDEREDQTY` int(11) NOT NULL,
+  `ORDEREDPRICE` double NOT NULL,
+  `ORDEREDNUM` int(11) NOT NULL,
+  `USERID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblorder`
+--
+
+INSERT INTO `tblorder` (`ORDERID`, `PROID`, `ORDEREDQTY`, `ORDEREDPRICE`, `ORDEREDNUM`, `USERID`) VALUES
+(4, 201740, 1, 149, 95, 0),
+(5, 201737, 1, 119, 96, 0),
+(6, 201739, 1, 289, 96, 0),
+(7, 201737, 1, 119, 97, 0),
+(8, 201739, 1, 289, 97, 0),
+(9, 201738, 1, 199, 98, 0),
+(11, 201761, 1, 3000, 100, 0),
+(12, 201761, 1, 3000, 101, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblproduct`
+--
+
+CREATE TABLE `tblproduct` (
+  `PROID` int(11) NOT NULL,
+  `PRODESC` varchar(255) DEFAULT NULL,
+  `INGREDIENTS` varchar(255) NOT NULL,
+  `PROQTY` int(11) DEFAULT NULL,
+  `ORIGINALPRICE` double NOT NULL,
+  `PROPRICE` double DEFAULT NULL,
+  `CATEGID` int(11) DEFAULT NULL,
+  `IMAGES` varchar(255) DEFAULT NULL,
+  `PROSTATS` varchar(30) DEFAULT NULL,
+  `OWNERNAME` varchar(90) NOT NULL,
+  `OWNERPHONE` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblproduct`
+--
+
+INSERT INTO `tblproduct` (`PROID`, `PRODESC`, `INGREDIENTS`, `PROQTY`, `ORIGINALPRICE`, `PROPRICE`, `CATEGID`, `IMAGES`, `PROSTATS`, `OWNERNAME`, `OWNERPHONE`) VALUES
+(201752, 'Clothes for sports and physical activities', '', 8, 120, 120, 19, 'uploaded_photos/img_682b1d97f09372.88280374.jpg', 'Available', 'Ourcompany', '09999999999'),
+(201753, 'Clothes for sports and physical activities', '', 4, 1800, 1500, 19, 'uploaded_photos/img_682b1e2f9559b5.74153647.jpg', 'Available', 'Ourcompany', '000000000000'),
+(201754, 'Clothing and accessories for men.', '', 5, 5000, 4000, 18, 'uploaded_photos/img_682b1ecd3b0af0.06900940.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201755, 'Clothing and accessories for men.', '', 6, 1000, 800, 18, 'uploaded_photos/img_682b1efe533229.09534679.jpg', 'Available', '', '09876543231'),
+(201756, 'Clothing and accessories for men.', '', 1, 4000, 3600, 18, 'uploaded_photos/img_682b1f48dcd139.40309831.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201757, 'Clothing and accessories for women', '', 4, 2000, 1500, 17, 'uploaded_photos/img_682b1f86b46ae6.02871888.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201758, 'Clothing and accessories for women\r\n', '', 4, 3200, 3000, 17, 'uploaded_photos/img_682b1fba8cc531.23845868.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201759, 'Containers to carry belongings.', '', 2, 3000, 2500, 11, 'uploaded_photos/img_682b200a9477a1.01090149.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201760, 'Containers to carry belongings.', '', 4, 1900, 1500, 11, 'uploaded_photos/img_682b207129ca85.70659568.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201761, 'Footwear for different activities and styles.', '', 10, 3000, 3000, 5, 'uploaded_photos/img_682b20c4831eb9.46798559.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201763, 'Footwear for different activities and styles.', '', 2, 2300, 2000, 5, 'uploaded_photos/img_682b2158c3f8f0.79700845.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201764, 'Clothes for everyday wear', '', 7, 200, 200, 12, 'uploaded_photos/img_682b21a8514571.37894507.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201765, 'Clothes for everyday wear', '', 5, 1200, 1200, 12, 'uploaded_photos/img_682b21eef37a76.34170008.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201766, ' Inside design and decoration of spaces', '', 4, 9000, 8000, 13, 'uploaded_photos/img_682b22287502c6.78691883.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201767, ' Inside design and decoration of spaces', '', 3, 11000, 10000, 13, 'uploaded_photos/img_682b2274707461.93769780.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201768, 'Items used at home.', '', 60, 300, 200, 14, 'uploaded_photos/img_682b22bca0d4c0.29317823.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201769, 'Items used at home.', '', 6, 90, 84, 14, 'uploaded_photos/img_682b22eae49942.26587967.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201770, 'Trendy and stylish clothing and accessories.\r\n', '', 4, 7000, 6500, 15, 'uploaded_photos/img_682b231e14bd91.12465632.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201771, 'Trendy and stylish clothing and accessories.\r\n', '', 5, 8000, 5500, 15, 'uploaded_photos/img_682b23670b0803.61816576.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201772, ' Clothes and essentials for children.', '', 3, 1000, 955, 16, 'uploaded_photos/img_682b23af5c30c4.20531424.jpg', 'Available', 'Ourcompany', '0987654321'),
+(201773, ' Clothes and essentials for children.', '', 4, 3200, 3000, 16, 'uploaded_photos/img_682b23e80e5459.05224642.jpg', 'Available', 'Ourcompany', '0987654321');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpromopro`
+--
+
+CREATE TABLE `tblpromopro` (
+  `PROMOID` int(11) NOT NULL,
+  `PROID` int(11) NOT NULL,
+  `PRODISCOUNT` double NOT NULL,
+  `PRODISPRICE` double NOT NULL,
+  `PROBANNER` tinyint(4) NOT NULL,
+  `PRONEW` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpromopro`
+--
+
+INSERT INTO `tblpromopro` (`PROMOID`, `PROID`, `PRODISCOUNT`, `PRODISPRICE`, `PROBANNER`, `PRONEW`) VALUES
+(1, 201737, 0, 119, 0, 0),
+(2, 201738, 0, 199, 0, 0),
+(3, 201739, 0, 289, 0, 0),
+(4, 201740, 0, 149, 0, 0),
+(5, 201741, 0, 89, 0, 0),
+(6, 201742, 0, 287, 0, 0),
+(13, 201749, 0, 189, 0, 0),
+(14, 201750, 0, 1500, 0, 0),
+(16, 201752, 0, 120, 0, 0),
+(17, 201753, 0, 1500, 0, 0),
+(18, 201754, 0, 4000, 0, 0),
+(19, 201755, 0, 800, 0, 0),
+(20, 201756, 0, 3600, 0, 0),
+(21, 201757, 0, 1500, 0, 0),
+(22, 201758, 0, 3000, 0, 0),
+(23, 201759, 0, 2500, 0, 0),
+(24, 201760, 0, 1500, 0, 0),
+(25, 201761, 0, 3000, 0, 0),
+(27, 201763, 0, 2000, 0, 0),
+(28, 201764, 0, 200, 0, 0),
+(29, 201765, 0, 1200, 0, 0),
+(30, 201766, 0, 8000, 0, 0),
+(31, 201767, 0, 10000, 0, 0),
+(32, 201768, 0, 200, 0, 0),
+(33, 201769, 0, 84, 0, 0),
+(34, 201770, 0, 6500, 0, 0),
+(35, 201771, 0, 5500, 0, 0),
+(36, 201772, 0, 955, 0, 0),
+(37, 201773, 0, 3000, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsetting`
+--
+
+CREATE TABLE `tblsetting` (
+  `SETTINGID` int(11) NOT NULL,
+  `PLACE` text NOT NULL,
+  `CITY` varchar(90) NOT NULL,
+  `DELPRICE` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblsetting`
+--
+
+INSERT INTO `tblsetting` (`SETTINGID`, `PLACE`, `CITY`, `DELPRICE`) VALUES
+(1, 'Addis Ababa', 'Addis Ababa', 50),
+(2, 'Other City', 'Other City', 70);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstockin`
+--
+
+CREATE TABLE `tblstockin` (
+  `STOCKINID` int(11) NOT NULL,
+  `STOCKDATE` datetime DEFAULT NULL,
+  `PROID` int(11) DEFAULT NULL,
+  `STOCKQTY` int(11) DEFAULT NULL,
+  `STOCKPRICE` double DEFAULT NULL,
+  `USERID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsummary`
+--
+
+CREATE TABLE `tblsummary` (
+  `SUMMARYID` int(11) NOT NULL,
+  `ORDEREDDATE` datetime NOT NULL,
+  `CUSTOMERID` int(11) NOT NULL,
+  `ORDEREDNUM` int(11) NOT NULL,
+  `DELFEE` double NOT NULL,
+  `PAYMENT` double NOT NULL,
+  `PAYMENTMETHOD` varchar(30) NOT NULL,
+  `ORDEREDSTATS` varchar(30) NOT NULL,
+  `ORDEREDREMARKS` varchar(125) NOT NULL,
+  `CLAIMEDADTE` datetime NOT NULL,
+  `HVIEW` tinyint(4) NOT NULL,
+  `USERID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblsummary`
+--
+
+INSERT INTO `tblsummary` (`SUMMARYID`, `ORDEREDDATE`, `CUSTOMERID`, `ORDEREDNUM`, `DELFEE`, `PAYMENT`, `PAYMENTMETHOD`, `ORDEREDSTATS`, `ORDEREDREMARKS`, `CLAIMEDADTE`, `HVIEW`, `USERID`) VALUES
+(11, '2025-05-19 02:43:05', 16, 100, 50, 3000, 'Cash on Delivery', 'Pending', 'Your order is on process.', '0000-00-00 00:00:00', 1, 0),
+(12, '2025-05-19 04:13:28', 16, 101, 0, 3000, 'Cash on Delivery', 'Pending', 'Your order is on process.', '0000-00-00 00:00:00', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluseraccount`
+--
+
+CREATE TABLE `tbluseraccount` (
+  `USERID` int(11) NOT NULL,
+  `U_NAME` varchar(122) NOT NULL,
+  `U_USERNAME` varchar(122) NOT NULL,
+  `U_PASS` varchar(122) NOT NULL,
+  `U_ROLE` varchar(30) NOT NULL,
+  `USERIMAGE` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbluseraccount`
+--
+
+INSERT INTO `tbluseraccount` (`USERID`, `U_NAME`, `U_USERNAME`, `U_PASS`, `U_ROLE`, `USERIMAGE`) VALUES
+(129, 'solen', 'solen', 'f865b53623b121fd34ee5426c792e5c33af8c227', 'Administrator', ''),
+(130, 'Sisiyo', 'siso', '4604de073493cfba750732571d7e21b7e6f8bd96', 'Staff', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblwishlist`
+--
+
+CREATE TABLE `tblwishlist` (
+  `id` int(11) NOT NULL,
+  `CUSID` int(11) NOT NULL,
+  `PROID` int(11) NOT NULL,
+  `WISHDATE` date NOT NULL,
+  `WISHSTATS` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `messagein`
+--
+ALTER TABLE `messagein`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `messagelog`
+--
+ALTER TABLE `messagelog`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `IDX_MessageId` (`MessageId`,`SendTime`);
+
+--
+-- Indexes for table `messageout`
+--
+ALTER TABLE `messageout`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `IDX_IsRead` (`IsRead`);
+
+--
+-- Indexes for table `tblautonumber`
+--
+ALTER TABLE `tblautonumber`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`CATEGID`);
+
+--
+-- Indexes for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  ADD PRIMARY KEY (`CUSTOMERID`);
+
+--
+-- Indexes for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  ADD PRIMARY KEY (`ORDERID`),
+  ADD KEY `USERID` (`USERID`),
+  ADD KEY `PROID` (`PROID`),
+  ADD KEY `ORDEREDNUM` (`ORDEREDNUM`);
+
+--
+-- Indexes for table `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  ADD PRIMARY KEY (`PROID`),
+  ADD KEY `CATEGID` (`CATEGID`);
+
+--
+-- Indexes for table `tblpromopro`
+--
+ALTER TABLE `tblpromopro`
+  ADD PRIMARY KEY (`PROMOID`),
+  ADD UNIQUE KEY `PROID` (`PROID`);
+
+--
+-- Indexes for table `tblsetting`
+--
+ALTER TABLE `tblsetting`
+  ADD PRIMARY KEY (`SETTINGID`);
+
+--
+-- Indexes for table `tblstockin`
+--
+ALTER TABLE `tblstockin`
+  ADD PRIMARY KEY (`STOCKINID`),
+  ADD KEY `PROID` (`PROID`,`USERID`),
+  ADD KEY `USERID` (`USERID`);
+
+--
+-- Indexes for table `tblsummary`
+--
+ALTER TABLE `tblsummary`
+  ADD PRIMARY KEY (`SUMMARYID`),
+  ADD UNIQUE KEY `ORDEREDNUM` (`ORDEREDNUM`),
+  ADD KEY `CUSTOMERID` (`CUSTOMERID`),
+  ADD KEY `USERID` (`USERID`);
+
+--
+-- Indexes for table `tbluseraccount`
+--
+ALTER TABLE `tbluseraccount`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- Indexes for table `tblwishlist`
+--
+ALTER TABLE `tblwishlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messagein`
+--
+ALTER TABLE `messagein`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `messagelog`
+--
+ALTER TABLE `messagelog`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `messageout`
+--
+ALTER TABLE `messageout`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblautonumber`
+--
+ALTER TABLE `tblautonumber`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `CATEGID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  MODIFY `CUSTOMERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  MODIFY `ORDERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tblpromopro`
+--
+ALTER TABLE `tblpromopro`
+  MODIFY `PROMOID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `tblsetting`
+--
+ALTER TABLE `tblsetting`
+  MODIFY `SETTINGID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblstockin`
+--
+ALTER TABLE `tblstockin`
+  MODIFY `STOCKINID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblsummary`
+--
+ALTER TABLE `tblsummary`
+  MODIFY `SUMMARYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbluseraccount`
+--
+ALTER TABLE `tbluseraccount`
+  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=131;
+
+--
+-- AUTO_INCREMENT for table `tblwishlist`
+--
+ALTER TABLE `tblwishlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
